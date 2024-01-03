10af27efcd70e46e12c3785bbe7d3121b39bceb9
Mehdi <mehdi.rabbai@gmail.com>
2024-01-02 17:59:27 -0800

    add correction for task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,7 +5,6 @@
 
 import requests
 from sys import argv, exit
-import csv
 
 
 def main():
@@ -14,19 +13,14 @@ def main():
     response_todos = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos")
     USERNAME = response_user.json()["name"]
-    LIST_TASKS = []
-    for task in response_todos.json():
-        DATA = {}
-        DATA["USER_ID"] = task["userId"]
-        DATA["USERNAME"] = USERNAME
-        DATA["TASK_COMPLETED_STATUS"] = task["completed"]
-        DATA["TASK_TITLE"] = task["title"]
-        LIST_TASKS.append(DATA)
-
-    with open(f"{argv[1]}.csv", "w", newline="") as out:
-        dict_writer = csv.DictWriter(out, LIST_TASKS[0].keys())
-        dict_writer.writeheader()
-        dict_writer.writerows(LIST_TASKS)
+    with open(f"{argv[1]}.csv", "w") as out:
+        for task in response_todos.json():
+            out.write(
+                '"{}","{}","{}","{}"\n'.format(
+                    argv[1],
+                    USERNAME,
+                    task['completed'],
+                    task['title']))
 
 
 if __name__ == "__main__":
