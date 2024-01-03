2fd89acdcc28cc89b10076933064c0e115f42d16
Mehdi <mehdi.rabbai@gmail.com>
2024-01-02 18:15:17 -0800

    add task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Accessing a RESTAPI for todo lists of employees using requests
+   and exporting data as a json file
+"""
+
+import requests
+from sys import argv, exit
+import json
+
+
+def main():
+    response_user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    response_todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos")
+    USERNAME = response_user.json()["username"]
+    USER_ID = argv[1]
+    USER_DICT = {}
+    USER_DICT[USER_ID] = []
+    for task in response_todos.json():
+        USER_DICT[USER_ID].append(
+            {"task": task["title"], "completed": task["completed"], "username": USERNAME})
+    with open(f"{USER_ID}.json", "w") as out:
+        json.dump(USER_DICT, out)
+
+
+if __name__ == "__main__":
+    main()
