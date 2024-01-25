e1a56b5b404138fc291dffe86f15064aff1295b7
Mehdi <mehdi.rabbai@gmail.com>
2024-01-25 15:34:42 -0800

    puppet-lint correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,11 @@
 
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin/'
-	}
+    path  => '/usr/local/bin/:/bin/'
+  }
 
-	# Restart Nginx
-	-> exec { 'nginx-restart':
-	  command => 'nginx restart',
-	    path    => '/etc/init.d/'
-		}
+  # Restart Nginx
+  -> exec { 'nginx-restart':
+    command => 'nginx restart',
+      path  => '/etc/init.d/'
+    }
